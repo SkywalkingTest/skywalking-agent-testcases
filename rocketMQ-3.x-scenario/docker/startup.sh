f5151beab5c8abd4dbb37470842bb23eb01bba67
carlvine <carlvine500@163.com>
2018-02-24 15:37:03 +0800

    rocketmq-3.x avoid virtual-ip option

--- rocketMQ-3.x-scenario/docker/startup.sh
+++ rocketMQ-3.x-scenario/docker/startup.sh
@@ -12,4 +12,4 @@ if [ -f "${AGENT_FILE_PATH}/skywalking-agent.jar" ]; then
     ROCKETMQ_PROVIDER_OPTS=" -javaagent:${AGENT_FILE_PATH}/skywalking-agent.jar -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dcollector.discovery_check_interval=2 -Dskywalking.collector.servers=${COLLECTOR_SERVER} -Dskywalking.agent.application_code=rocketMQ-scenario "
 fi
 
-java $ROCKETMQ_PROVIDER_OPTS -DrocketMQ.nameSrv=${ROCKETMQ_NAMESRV} -jar ${ROCKETMQ_SCENARIO_HOME}/rocketMQ-scenario/rocketMQ-scenario.jar
+java $ROCKETMQ_PROVIDER_OPTS -DrocketMQ.nameSrv=${ROCKETMQ_NAMESRV} -Dcom.rocketmq.sendMessageWithVIPChannel=false -jar ${ROCKETMQ_SCENARIO_HOME}/rocketMQ-scenario/rocketMQ-scenario.jar
