0557593472a8f34dce541378550767bf02c12e87
Serena <Serena>
2018-08-11 15:03:12 +0800

    add forward test case

--- undertow-scenario/src/main/java/org/apache/skywalking/testcase/undertow/controller/CaseForwardController.java
+++ undertow-scenario/src/main/java/org/apache/skywalking/testcase/undertow/controller/CaseForwardController.java
@@ -5,32 +5,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Copyright @ 2018/8/11
  *
  * @author cloudgc
  */
 @Controller
+@RequestMapping("/case")
 public class CaseForwardController {
 
      private final static Logger log = LoggerFactory.getLogger(CaseForwardController.class);
 
     @RequestMapping("/forwardone")
-    public String startForwardOne(boolean nodeOne){
-
-        return "nodeOne";
+    public void startForwardOne(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log.info("request:{},response:{}",request,response);
+        request.getRequestDispatcher("/forwardTwo").forward(request,response);
     }
 
     @RequestMapping("/forwardTwo")
-    public String startForwardTwo(){
-
-        return "forwardTwo";
+    public void startForwardTwo(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        log.info("request:{},response:{}",request,response);
+        request.getRequestDispatcher("/forwardend").forward(request,response);
     }
     @RequestMapping("/forwardend")
     public String forardEnd(){
         return "forwardend";
     }
-
-
 
 }
