6df7c16ffd6d49b303ad1ed1b8c157fd493b4d6a
ascrutae <nzt48.xin@gmail.com>
2018-11-20 19:49:20 +0800

    Change the application_code

--- header-scenario/projectA/docker/start.sh
+++ header-scenario/projectA/docker/start.sh
@@ -2,7 +2,7 @@
 
 JAVA_OPTS=" -javaagent:/usr/local/header-scenario/apache-skywalking-apm-incubating/agent/skywalking-agent.jar "
 JAVA_OPTS="$JAVA_OPTS -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dskywalking.collector.discovery_check_interval=2 -Dskywalking.collector.servers=${COLLECTOR_SERVER} "
-JAVA_OPTS="$JAVA_OPTS -Dskywalking.agent.application_code=projectA"
+JAVA_OPTS="$JAVA_OPTS -Dskywalking.agent.service_name=projectA"
 
 JAVA_OPTS="$JAVA_OPTS -Durl.prefix=http://${PROJECTB_URL}/projectB"
 
