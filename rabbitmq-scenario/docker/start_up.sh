63b3e05a4e5193f93d4a7f017600e751de103490
WithLin <withlin@yeah.net>
2018-11-30 15:26:49 +0800

    fix add rabbitmq  test case

---
+++ rabbitmq-scenario/docker/start_up.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RABBITMQ_SCENARIO_HOME=/usr/local/rabbitmq-scenario
+
+
+export AGENT_FILE_PATH=${RABBITMQ_SCENARIO_HOME}/agent
+if [ -f "${AGENT_FILE_PATH}/skywalking-agent.jar" ]; then
+    RABBITMQ_SCENARIO_OPTS=" -javaagent:${AGENT_FILE_PATH}/skywalking-agent.jar -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dcollector.discovery_check_interval=2 -Dskywalking.collector.servers=${COLLECTOR_SERVER} -Dskywalking.agent.application_code=rabbitmq-scenario "
+fi
+
+java ${RABBITMQ_SCENARIO_OPTS} -Drabbitmq.host=${RABBITMQ_HOST} -jar ${RABBITMQ_SCENARIO_HOME}/rabbitmq-scenario.jar
