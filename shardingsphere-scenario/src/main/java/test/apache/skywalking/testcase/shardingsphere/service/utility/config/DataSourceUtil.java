58873b0f47f4155ff11eb5d420af2375578e8c1e
tuohai666 <zhangyonglun@apache.org>
2019-01-17 20:25:15 +0800

    for comments

--- shardingsphere-scenario/src/main/java/test/apache/skywalking/testcase/shardingsphere/service/utility/config/DataSourceUtil.java
+++ shardingsphere-scenario/src/main/java/test/apache/skywalking/testcase/shardingsphere/service/utility/config/DataSourceUtil.java
@@ -31,8 +31,6 @@ public class DataSourceUtil {
     
     private static final String HOST = System.getProperty("mysql.host");
     
-    private static final int PORT = 3306;
-    
     private static final String USER_NAME = "root";
     
     private static final String PASSWORD = "root";
@@ -45,7 +43,7 @@ public class DataSourceUtil {
         System.out.println("host: " + HOST);
         BasicDataSource result = new BasicDataSource();
         result.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
-        result.setUrl(String.format("jdbc:mysql://%s:%s/%s", HOST, PORT, dataSourceName));
+        result.setUrl(String.format("jdbc:mysql://%s/%s", HOST, dataSourceName));
         result.setUsername(USER_NAME);
         result.setPassword(PASSWORD);
         datasourceMap.put(dataSourceName, result);
