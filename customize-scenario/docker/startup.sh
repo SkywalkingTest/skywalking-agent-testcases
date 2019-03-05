021cadebd5449f5b18629e88f3f9e87b1c229a04
赵禹光 <zhaoyuguang@lianjia.com>
2019-03-05 16:16:35 +0800

    Fix docker desc file issue.

--- customize-scenario/docker/startup.sh
+++ customize-scenario/docker/startup.sh
@@ -5,4 +5,4 @@ if [ -f "${AGENT_FILE_PATH}/skywalking-agent.jar" ]; then
     CUSTOMIZE_OPTS=" -javaagent:${AGENT_FILE_PATH}/skywalking-agent.jar -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dcollector.discovery_check_interval=2 -Dskywalking.collector.backend_service=${COLLECTOR_SERVER} -Dskywalking.agent.service_name=customize-scenario -Dskywalking.plugin.customize.trace_dsl=true -Xms256m -Xmx256m -XX:PermSize=64M -XX:MaxPermSize=64"
 fi
 
-java $CUSTOMIZE_OPTS -jar /usr/local/skywalking-customize-scenario/customize-scenario.jar
+java $CUSTOMIZE_OPTS -Dskywalking.customize.enhance_file=/usr/local/skywalking-customize-scenario/customize_enhance.xml -jar /usr/local/skywalking-customize-scenario/customize-scenario.jar
