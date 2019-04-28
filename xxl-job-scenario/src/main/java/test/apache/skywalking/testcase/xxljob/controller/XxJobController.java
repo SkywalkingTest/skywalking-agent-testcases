bc7221b9238de529f94abdae9b64f6492f9f4d49
tianjunwei <tianjunwei@mi-ya.com.cn>
2019-04-28 17:17:10 +0800

    add support xxljob

--- xxl-job-scenario/src/main/java/test/apache/skywalking/testcase/xxljob/controller/XxJobController.java
+++ xxl-job-scenario/src/main/java/test/apache/skywalking/testcase/xxljob/controller/XxJobController.java
@@ -22,21 +22,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 import test.apache.skywalking.testcase.xxljob.handler.DemoJobHandler;
-
-import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
 @RequestMapping("/requestmethod")
 @PropertySource("classpath:application.properties")
-public class SpringEndpointRequestMethodNameController {
+public class XxJobController {
 
-    private Logger logger = LoggerFactory.getLogger(SpringEndpointRequestMethodNameController.class);
+    private Logger logger = LoggerFactory.getLogger(XxJobController.class);
 
     @Autowired
     private DemoJobHandler demoJobHandler;
