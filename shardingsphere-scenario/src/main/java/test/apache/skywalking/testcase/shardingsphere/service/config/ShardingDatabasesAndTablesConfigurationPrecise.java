a918ada3c0c63cb02990bc3745124d2902f4707f
tuohai666 <zhangyonglun@apache.org>
2019-05-06 16:32:17 +0800

    for test

--- shardingsphere-scenario/src/main/java/test/apache/skywalking/testcase/shardingsphere/service/config/ShardingDatabasesAndTablesConfigurationPrecise.java
+++ shardingsphere-scenario/src/main/java/test/apache/skywalking/testcase/shardingsphere/service/config/ShardingDatabasesAndTablesConfigurationPrecise.java
@@ -44,7 +44,9 @@ public final class ShardingDatabasesAndTablesConfigurationPrecise implements Exa
         shardingRuleConfig.getBindingTableGroups().add("t_order, t_order_item");
         shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(new InlineShardingStrategyConfiguration("user_id", "demo_ds_${user_id % 2}"));
         shardingRuleConfig.setDefaultTableShardingStrategyConfig(new StandardShardingStrategyConfiguration("order_id", new PreciseModuloShardingTableAlgorithm()));
-        dataSource = ShardingDataSourceFactory.createDataSource(createDataSourceMap(), shardingRuleConfig, new HashMap<String, Object>(), new Properties());
+        Properties properties = new Properties();
+        properties.setProperty("max.connections.size.per.query", 16);
+        dataSource = ShardingDataSourceFactory.createDataSource(createDataSourceMap(), shardingRuleConfig, new HashMap<String, Object>(), properties);
         return dataSource;
     }
     
