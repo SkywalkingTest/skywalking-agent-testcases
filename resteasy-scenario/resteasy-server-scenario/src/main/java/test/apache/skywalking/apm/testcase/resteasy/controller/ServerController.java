9cdb0be0b2daf73999b6fad04b43a10704f91bdd
yanfch <yanfch.china@gmail.com>
2019-05-14 21:08:47 +0800

    fix(resteasy-scenario): fix file license

--- resteasy-scenario/resteasy-server-scenario/src/main/java/test/apache/skywalking/apm/testcase/resteasy/controller/ServerController.java
+++ resteasy-scenario/resteasy-server-scenario/src/main/java/test/apache/skywalking/apm/testcase/resteasy/controller/ServerController.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package test.apache.skywalking.apm.testcase.resteasy.controller;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
@@ -34,8 +52,7 @@ public class ServerController {
                 try {
                     Thread.sleep(100);
                     response.resume(Response.ok("Hello Async!").type(MediaType.TEXT_PLAIN).build());
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } catch (Exception ignored) {
                 }
             }
         }).start();
@@ -53,7 +70,6 @@ public class ServerController {
         target = client.target("http://localhost:18080/resteasy-server-case/case/async");
         Response response1 = target.request().get();
         response1.close();
-
         return "success";
     }
 }
