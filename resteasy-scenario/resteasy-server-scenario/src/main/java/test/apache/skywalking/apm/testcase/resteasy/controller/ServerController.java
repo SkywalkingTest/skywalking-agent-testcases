7868bd48cdf93366f3bf4fe48252085d2e9a8427
yanfch <yanfch.china@gmail.com>
2019-05-22 12:40:57 +0800

    fix(resteasy-scenario): fix test case

--- resteasy-scenario/resteasy-server-scenario/src/main/java/test/apache/skywalking/apm/testcase/resteasy/controller/ServerController.java
+++ resteasy-scenario/resteasy-server-scenario/src/main/java/test/apache/skywalking/apm/testcase/resteasy/controller/ServerController.java
@@ -18,17 +18,9 @@
 
 package test.apache.skywalking.apm.testcase.resteasy.controller;
 
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.container.AsyncResponse;
-import javax.ws.rs.container.Suspended;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 /**
  * @author yan-fucheng
@@ -41,35 +33,5 @@ public class ServerController {
     @Produces("text/plain")
     public String syncRequest() {
         return "Hello Sync!";
-    }
-
-    @GET
-    @Path("async")
-    @Produces("text/plain")
-    public void asyncRequest(@Suspended final AsyncResponse response) {
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(100);
-                    response.resume(Response.ok("Hello Async!").type(MediaType.TEXT_PLAIN).build());
-                } catch (Exception ignored) {
-                }
-            }
-        }).start();
-    }
-
-    @GET
-    @Path("call")
-    @Produces("text/plain")
-    public String callAll() {
-        ResteasyClient client = new ResteasyClientBuilder().build();
-        ResteasyWebTarget target = client.target("http://localhost:18080/resteasy-server-case/case/sync");
-        Response response = target.request().get();
-        response.close();
-
-        target = client.target("http://localhost:18080/resteasy-server-case/case/async");
-        Response response1 = target.request().get();
-        response1.close();
-        return "success";
     }
 }
