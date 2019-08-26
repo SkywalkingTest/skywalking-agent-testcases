3c23e75d64b2bf6c8dfb93dfd0859b8de25d7a06
stone <stone.wlg@gmail.com>
2019-08-26 17:03:21 +0800

    cassandra 3.x plugin

--- cassandra-3.x-scenario/docker/startup.sh
+++ cassandra-3.x-scenario/docker/startup.sh
@@ -5,4 +5,5 @@ if [ -f "${AGENT_FILE_PATH}/skywalking-agent.jar" ]; then
     CASSANDRA_OPTS=" -javaagent:${AGENT_FILE_PATH}/skywalking-agent.jar -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dcollector.discovery_check_interval=2 -Dskywalking.collector.backend_service=${COLLECTOR_SERVER} -Dskywalking.agent.service_name=cassandra-scenario -Xms256m -Xmx256m -XX:PermSize=64M -XX:MaxPermSize=64"
 fi
 
-java $CASSANDRA_OPTS -DCASSANDRA.host=${CASSANDRA_HOST} -jar /usr/local/skywalking-cassandra-scenario/cassandra-scenario.jar
+java $CASSANDRA_OPTS -Dcassandra.host=${CASSANDRA_HOST} -Dcassandra.port=${CASSANDRA_PORT} -jar
+/usr/local/skywalking-cassandra-scenario/cassandra-scenario.jar
--- cassandra-3.x-scenario/src/main/java/test/apache/skywalking/testcase/cassandra/controller/CaseController.java
+++ cassandra-3.x-scenario/src/main/java/test/apache/skywalking/testcase/cassandra/controller/CaseController.java
@@ -1,47 +1,64 @@
 package test.apache.skywalking.testcase.cassandra.controller;
 
+import com.datastax.driver.core.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/case")
 public class CaseController {
 
+    private static final String CREATE_KEYSPACE_SQL = "CREATE KEYSPACE IF NOT EXISTS demo WITH replication = " +
+        "{'class': 'SimpleStrategy', 'replication_factor': 1}";
+    private static final String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS demo.test(id TEXT PRIMARY KEY, value TEXT)";
+    private static final String INSERT_DATA_SQL = "INSERT INTO demo.test(id, value) VALUES(?,?)";
+    private static final String SELECT_DATA_SQL = "SELECT * FROM demo.test WHERE id = ?";
+    private static final String DELETE_DATA_SQL = "DELETE FROM demo.test WHERE id = ?";
+    private static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS demo.test";
+    private static final String DROP_KEYSPACE = "DROP KEYSPACE IF EXISTS demo";
+    private Cluster cluster;
+    private Session session;
     private Logger logger = LogManager.getLogger(CaseController.class);
-    @Value("${cassandra.contact-points}")
-    private String contactPoints;
+    @Value("${cassandra.host}")
+    private String host;
+    @Value("${cassandra.port}")
+    private int port;
 
     @GetMapping("/cassandra")
-    public String mongoDBCase() {
-        logger.info("cassandra contact points: {} ", contactPoints);
-//        MongoClient mongoClient = new MongoClient(mongoDBHost, 27017);
-//        MongoDatabase db = mongoClient.getDatabase("test-database");
-//        db.createCollection("testCollection");
-//        try {
-//            MongoCollection<Document> collection = db.getCollection("testCollection");
-//            Document document = Document.parse("{id: 1, name: \"test\"}");
-//            collection.insertOne(document);
-//
-//            FindIterable<Document> findIterable = collection.find(eq("name", "test"));
-//            Document findDocument = findIterable.first();
-//            logger.info("find id[{}] document, and the name is {}", findDocument.get("id"), findDocument.get("name"));
-//
-//            collection.updateOne(eq("name", "test"), BsonDocument.parse("{ $set : { \"name\": \"testA\"} }"));
-//
-//            findIterable = collection.find(eq("name", "testA"));
-//            findDocument = findIterable.first();
-//            logger.info("find id[{}] document, and the name is {}", findDocument.get("id"), findDocument.get("name"));
-//
-//            collection.deleteOne(eq("id", "1"));
-//        } finally {
-//            mongoClient.dropDatabase("test-database");
-//        }
+    public String cassandraCase() {
+        logger.info("cassandra contact points: {}:{}", host, port);
 
-        return "success";
+        try {
+            cluster = Cluster.builder().addContactPoint(host).withPort(port).build();
+            session = cluster.connect();
+
+            session.execute(CREATE_KEYSPACE_SQL);
+            session.execute(CREATE_TABLE_SQL);
+
+            PreparedStatement insertDataPreparedStatement = session.prepare(INSERT_DATA_SQL);
+            ResultSet insertDataResultSet = session.execute(insertDataPreparedStatement.bind("101", "foobar"));
+            logger.info(insertDataResultSet.toString());
+
+            PreparedStatement selectDataPreparedStatement = session.prepare(SELECT_DATA_SQL);
+            ResultSet resultSet = session.execute(selectDataPreparedStatement.bind("101"));
+            Row row = resultSet.one();
+            logger.info("id: {}, value: {}", row.getString("id"), row.getString("value"));
+
+            PreparedStatement deleteDataPreparedStatement = session.prepare(DELETE_DATA_SQL);
+            ResultSet deleteDataResultSet = session.execute(deleteDataPreparedStatement.bind("101"));
+            logger.info(deleteDataResultSet.toString());
+
+            session.execute(DROP_TABLE_SQL);
+            session.execute(DROP_KEYSPACE);
+        } finally {
+            session.close();
+            cluster.close();
+        }
+
+        return "ok";
     }
 }
